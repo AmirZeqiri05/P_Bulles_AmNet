c0a4b021f2b356c58ac374946c3229a5b6b0d9db
Amir Zeqiri <amir.zeqiri@eduvaud.ch>
2024-01-24 15:13:53 +0100

    Avant redemarrage

--- P_Bulles_Cloud/Form1.Designer.cs
+++ P_Bulles_Cloud/Form1.Designer.cs
@@ -28,12 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(240, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(186, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Mes fichiers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(240, 287);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(186, 53);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Mes fichiers";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(155, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(365, 80);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AmNet Cloud";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(709, 423);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AmNet Cloud";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
     }
 }
--- P_Bulles_Cloud/Form1.cs
+++ P_Bulles_Cloud/Form1.cs
@@ -6,5 +6,25 @@ namespace P_Bulles_Cloud
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
